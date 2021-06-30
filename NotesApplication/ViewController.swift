e5ad4c44ce2bee83c06c8f326cc3b5905cb10655
duyphamvu47 <50992400+duyphamvu47@users.noreply.github.com>
2021-06-30 02:13:52 -0700

    Add searching for note

--- NotesApplication/ViewController.swift
+++ NotesApplication/ViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet var table:UITableView!
     @IBOutlet var label:UILabel!
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var searchResult:[Note] = []
+    var isSearching:Bool = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.loadNote()
@@ -41,13 +46,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     // Table func:
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isSearching{
+            return searchResult.count
+        }
         return noteList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = noteList[indexPath.row].title
-        cell.detailTextLabel?.text = noteList[indexPath.row].note
+        if isSearching{
+            cell.textLabel?.text = searchResult[indexPath.row].title
+            cell.detailTextLabel?.text = searchResult[indexPath.row].note
+        }
+        else{
+            cell.textLabel?.text = noteList[indexPath.row].title
+            cell.detailTextLabel?.text = noteList[indexPath.row].note
+        }
         return cell
     }
     
@@ -55,12 +69,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let noteToDisplay = noteList[indexPath.row]
-        
         //Show note controller
         guard let viewController = storyboard?.instantiateViewController(identifier: "note") as? NotesViewController else{
             return
         }
+        
+        var noteToDisplay:Note = Note(title: "", note: "")
+        let index:Int = indexPath.row
+
+        if isSearching{
+            noteToDisplay = searchResult[indexPath.row]
+        }
+        else{
+            noteToDisplay = noteList[indexPath.row]
+        }
+        
         viewController.navigationItem.largeTitleDisplayMode = .never
         viewController.title = "Note"
         viewController.noteTitle = noteToDisplay.title
@@ -68,25 +91,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         viewController.completion = {title, note in
             self.navigationController?.popToRootViewController(animated: true)
             let newNote = Note(title: title, note: note)
-
-            if let row = noteList.firstIndex(where: {$0.title == title || $0.note == note}) {
-                   noteList[row] = newNote
-            }
-            else{
-                noteList.append(newNote)
+            
+            if self.isSearching{
+                if let row = noteList.firstIndex(where: {$0.title.lowercased() == noteToDisplay.title || $0.note.lowercased() == noteToDisplay.note}){
+                    
+                    noteList[row] = newNote
+                }
+                
+                self.searchResult[index] = newNote
+                
+            }else{
+                noteList[index] = newNote
             }
             self.label.isHidden = true
             self.table.isHidden = false
             self.saveNote()
             self.loadNote()
+            
+            if self.isSearching{
+                self.table.reloadData()
+            }
         }
         navigationController?.pushViewController(viewController, animated: true)
     }
     
     // delete cell
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
         if editingStyle == .delete{
-            noteList.remove(at: indexPath.row)
+            if isSearching{
+                if let index = noteList.firstIndex(where: {$0.title.lowercased() == searchResult[indexPath.row].title.lowercased()}){
+                    
+                    noteList.remove(at: index)
+                }
+                searchResult.remove(at: indexPath.row)
+            }else{
+                noteList.remove(at: indexPath.row)
+            }
             self.saveNote()
             self.loadNote()
         }
@@ -115,7 +156,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
         print("LoadData: ", noteList)
         self.table.reloadData()
-        
+    }
+}
+
+extension ViewController: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if(searchText.count > 0){
+            searchResult = noteList.filter({$0.title.lowercased().contains(searchText.lowercased()) ||
+                $0.note.lowercased().contains(searchText.lowercased())})
+            isSearching = true
+        }else{
+            isSearching = false
+        }
+        table.reloadData()
+
     }
 }
 
